4d52a287c89e6ade52038360c19cf64672b3b8e1
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-18 22:47:35 +0100

    Create an object with selected ingredient data

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 import {recipeData} from './recipe-data.js';
 const listIngredients = document.querySelector('.list__ingredients');
-const myBag = [];
+let listIngredientsArray = [];
+let myBag = [];
 console.log(recipeData);
 
-function createIngredienteElement (ingredient) {
+function createIngredienteElement (ingredient, index) {
   return `
-    <li data-item-type="${ingredient.product}" class="list-group-item list__item">
+    <li id="${index}" class="list-group-item list__item">
       <div class="item__input-wrapper" >
-        <input data-item-type="${ingredient.product}" class="item__checkbox" type="checkbox" aria-label="Checkbox for following text input">
+        <input data-item-type="${index}" class="item__checkbox" type="checkbox" aria-label="Checkbox for following text input">
         <input data-item-type="${ingredient.product}" type="number" class="input__item-number" aria-label="Text input with checkbox">
         <ul class="item__list">
           <li class="item__description">Artículo: ${ingredient.product}</li>
@@ -24,15 +25,34 @@ function createIngredienteElement (ingredient) {
 
 function handleChangeCheckbox (event) {
   const checkboxSelected = event.currentTarget;
-  console.log('soy el checkbox seleccinado');
-  // checkboxSelected.checked?  : '';
+  const checkboxIndex = checkboxSelected.getAttribute('data-item-type');
+
+  if (checkboxSelected.checked) {
+    console.log('estoy checkeado');
+    const amountInput = checkboxSelected.nextElementSibling;
+    listIngredientsArray[checkboxIndex].amount = amountInput.value;
+  }
+  else {
+    listIngredientsArray[checkboxIndex].amount = 0;
+  }
+
+  console.log(listIngredientsArray);
 }
 
-for (let item of recipeData.recipe.ingredients) {
-  const ingredientElement = createIngredienteElement(item);
+recipeData.recipe.ingredients.forEach((ingredient, index) => {
+  const ingredientElement = createIngredienteElement(ingredient, index);
   listIngredients.innerHTML += ingredientElement;
+  listIngredientsArray.push(
+    {
+      name: ingredient.product,
+      amount: 0, 
+      price: ingredient.price,
+    }
+  )
+})
 
-}
+console.log(listIngredientsArray);
+
 
 const allCheckbox = document.querySelectorAll('.item__checkbox');
 for (let checkbox of allCheckbox) {
